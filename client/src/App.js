22c963572e914ab83d1ccb8dd7208904719bb588
Aone8080 <aoneport8080@gmail.com>
2023-10-25 11:57:48 +0700

    25/10/2566

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 // Layout
 import MyNavbar from "./Component/Navbar/MyNavbar";
 
-
 // functions
 import { currentUser } from "./functions/auth";
 //Page
@@ -18,7 +17,6 @@ import HomeAdmin from "./Page/HomeAdmin/HomeAdmin";
 import ImportCourse from "./Page/ImportCourse/ImportCourse";
 import ImportStudent from "./Page/ImportStudent/ImportStudent";
 import BookingApproval from "./Page/BookingApproval/BookingApproval";
-
 import ManagementRoomType from './Component/Management/ManagementRoomType'
 import ManagementRoom from './Component/Management/ManagementRoom'
 import ManagementSubject from './Component/Management/ManagementSubject'
@@ -28,9 +26,6 @@ import ManagementCourse from './Component/Management/ManagementCourse'
 import ManagementStdInCourse from './Component/Management/ManagementStdInCourse'
 import ManagementYearsTerm from './Component/Management/ManagementYearsTerm'
 import ManagementUser from './Component/Management/ManagementUser'
-
-
-
 
 //Protect Routes
 import UserRoute from "./Protect_Rout/UserRoute"; 
@@ -59,8 +54,6 @@ function App() {
         console.log(err);
       });
 
-      
-      
   }
 
   return (
@@ -68,33 +61,30 @@ function App() {
       <MyNavbar />
       <Routes>
         {/* user */}
-        <Route path="/"                 element={<Home/>} />
-        <Route path="/login"            element={<Login />} />
-        <Route path="/user/index"       element={<UserRoute>  <HomeUser /> </UserRoute>}/>
-        <Route path="/calendar"         element={<MyCalendar />} />
-        <Route path="/booking"          element={<Booking />} />
-        <Route path="/booking/:id"      element={<SinglePageBooking />} />
-        <Route path="/booking-history"  element={<BookingHistory />} />
-        <Route path="/edit-profile"     element={<EditProfile />} />
+        <Route path="/"                           element={<Home/>} />
+        <Route path="/login"                      element={<Login />} />
+        <Route path="/calendar"                   element={<MyCalendar />} />
+        <Route path="/edit-profile"               element={<EditProfile />} />
+        <Route path="/user/index"                 element={<UserRoute>  <HomeUser />              </UserRoute>}/>
+        <Route path="/booking"                    element={<UserRoute>  <Booking />               </UserRoute>}/>
+        <Route path="/booking/:id"                element={<UserRoute>  <SinglePageBooking />     </UserRoute>}/>
+        <Route path="/booking-history"            element={<UserRoute>  <BookingHistory />        </UserRoute>}/>
 
         {/* admin */}
-        <Route path="/admin/index"      element={<AdminRoute>  <HomeAdmin/>       </AdminRoute>}/>
-        <Route path="/importcourse"     element={<AdminRoute>   <ImportCourse/>    </AdminRoute>}/>
-        <Route path="/importstudent"    element={<AdminRoute>   <ImportStudent/>   </AdminRoute>}/>
-        <Route path="/booking-approval" element={<AdminRoute>   <BookingApproval/> </AdminRoute>}/>
+        <Route path="/admin/index"                element={<AdminRoute>  <HomeAdmin/>             </AdminRoute>}/>
+        <Route path="/importcourse"               element={<AdminRoute>  <ImportCourse/>          </AdminRoute>}/>
+        <Route path="/importstudent"              element={<AdminRoute>  <ImportStudent/>         </AdminRoute>}/>
+        <Route path="/booking-approval"           element={<AdminRoute>  <BookingApproval/>       </AdminRoute>}/>
+        <Route path="/data-managementroom"        element={<AdminRoute>  <ManagementRoom/>        </AdminRoute>}/>
+        <Route path="/data-managementroomtype"    element={<AdminRoute>  <ManagementRoomType/>    </AdminRoute>}/>
+        <Route path="/data-managementcourse"      element={<AdminRoute>  <ManagementCourse/>      </AdminRoute>}/>
+        <Route path="/data-managementlecturer"    element={<AdminRoute>  <ManagementLecturer/>    </AdminRoute>}/>
+        <Route path="/data-managementstdincourse" element={<AdminRoute>  <ManagementStdInCourse/> </AdminRoute>}/>
+        <Route path="/data-managementstudent"     element={<AdminRoute>  <ManagementStudent/>     </AdminRoute>}/>
+        <Route path="/data-managementsubject"     element={<AdminRoute>  <ManagementSubject/>     </AdminRoute>}/>
+        <Route path="/data-managementyearsterm"   element={<AdminRoute>  <ManagementYearsTerm/>   </AdminRoute>}/>
+        <Route path="/data-managementuser"        element={<AdminRoute>  <ManagementUser/>        </AdminRoute>}/>
 
-        <Route path="/data-managementroom"        element={<AdminRoute>   <ManagementRoom/>        </AdminRoute>}/>
-        <Route path="/data-managementroomtype"    element={<AdminRoute>   <ManagementRoomType/>    </AdminRoute>}/>
-        <Route path="/data-managementcourse"      element={<AdminRoute>   <ManagementCourse/>      </AdminRoute>}/>
-        <Route path="/data-managementlecturer"    element={<AdminRoute>   <ManagementLecturer/>    </AdminRoute>}/>
-        <Route path="/data-managementstdincourse" element={<AdminRoute>   <ManagementStdInCourse/> </AdminRoute>}/>
-        <Route path="/data-managementstudent"     element={<AdminRoute>   <ManagementStudent/>     </AdminRoute>}/>
-        <Route path="/data-managementsubject"     element={<AdminRoute>   <ManagementSubject/>     </AdminRoute>}/>
-        <Route path="/data-managementyearsterm"   element={<AdminRoute>   <ManagementYearsTerm/>   </AdminRoute>}/>
-        <Route path="/data-managementuser"        element={<AdminRoute>   <ManagementUser/>        </AdminRoute>}/>
-
-        
-       
     </Routes>
     </>
   );
